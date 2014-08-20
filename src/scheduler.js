94cdf7acd148fcd0bed05880cd8b937809d3e0a7
Austin Knight <austinknight89@gmail.com>
2014-08-20 16:05:37 -0600

    Auto load past and previous days

--- src/scheduler.js
+++ src/scheduler.js
@@ -9,17 +9,6 @@ function scheduler ($timeout) {
     controller: function ($scope) {
       var self = $scope;
 
-      // Set the days in a week
-      self.days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-
-      // Get the months in the year
-      var monthCount = 0;
-      var months = [];
-
-      while (monthCount < 12) months.push(moment().month(monthCount++).format("MMMM"));
-
-      self.months = months;
-
       // Get initial date range for days view
       var now = moment()
         , daysStart = moment().subtract(7, 'days')
@@ -28,28 +17,69 @@ function scheduler ($timeout) {
         , daysList = [];
 
 
-      var setRange = daysRange.by('days', function(moment) {
+      daysRange.by('days', function(moment) {
         daysList.push(moment.format("ddd, MMMM Do"));
       });
+
+      self.getPastDays = function () {
+        daysStart = daysStart.subtract(1, 'days');
+        self.daysList.unshift(daysStart.format("ddd, MMMM Do"));
+      }
+
+      self.getfutureDays = function () {
+        daysEnd = daysEnd.add(1, 'days');
+        self.daysList.push(daysEnd.format("ddd, MMMM Do"));
+      }
+
       self.daysList = daysList;
-      console.log(daysList);
+
+      self.now = now.format("ddd, MMMM Do");
+
+      self.loadMore = function() {
+        console.log('test')
+      }
     },
     link: function($scope, $elem, $attrs){
-      
-
       $timeout(function() {
-        var blockWrap = $($elem).find('.block-wrap')
+        // Set the width of the block container to scroll
+        var setWrapperWidth = function() {
+          var blockWrap = $($elem).find('.block-wrap')
           , blocks = $($elem).find('.block')
           , totalBlockWidths = 0;
+          
+          $.each(blocks, function(index, value) {
+            totalBlockWidths = totalBlockWidths + $(value).outerWidth();
+          });
 
-        console.log(totalBlockWidths);
+          blockWrap.width(totalBlockWidths);
+        };
+
+        setWrapperWidth();
+
+        // Set scroll position of schedule wrapper to current day on load
+        var currentDayPos = $('.current-day').position().left;
+
+        $('.outter-block-wrap').scrollLeft(currentDayPos - 20);
+
+        // Checking if we have scrolled to first/last blocks, then load in more days
+        var firstBlockPos = $('.first-block')
+          , lastBlockPos = $('.last-block');
         
-        $.each(blocks, function(index, value) {
-          totalBlockWidths = totalBlockWidths + $(value).outerWidth();
-        });
-        console.log(totalBlockWidths);
+        var wrapper = angular.element($elem[0].firstElementChild)
+          , raw = $elem[0].firstElementChild;
 
-        blockWrap.width(totalBlockWidths);
+        wrapper.bind('scroll', function() {
+          var maxScrollLeft = raw.scrollWidth - 1;
+          if (raw.scrollLeft + raw.offsetWidth <= raw.offsetWidth) {
+            $scope.$apply($scope.getPastDays);
+            $(wrapper).scrollLeft($('.block:nth-child(2)').position().left - 5);
+            setWrapperWidth();
+          } else if (raw.scrollLeft + raw.offsetWidth >= maxScrollLeft) {
+            $scope.$apply($scope.getfutureDays);
+            setWrapperWidth();
+          }
+        });
+
       }, 0);
     }
   };
